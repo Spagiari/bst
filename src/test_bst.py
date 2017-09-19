3fb965d2e41c0108e1e8e4fa67473d5bc533d5b8
Eduardo Spagiari <e.spagiari@gmail.com>
2017-09-19 09:52:47 -0300

    improve test bst

--- src/test_bst.py
+++ src/test_bst.py
@@ -1,3 +1,4 @@
+
 import sys
 import bst
 import string
@@ -47,6 +48,90 @@ def test_insert():
         data.put(i, str(i))
 
     j = 0
+    for i in data.get_all_keys():
+        assert(i == keys[j])
+        j += 1
+
+def test_insert_delete():
+    data = bst.BST()
+    keys = [0, 1, 2, 3, 4, 5 ,6 ,7 , 8, 9, 10]
+    keyso = [0, 1, 2, 3, 4, 6 ,7 , 8, 9, 10]
+
+    for i in keys:
+        data.put(i, str(i))
+
+    data.delete(5)
+    j = 0
+    for i in data.get_all_keys():
+        assert(i == keyso[j])
+        j += 1
+
+def test_insert_delete_2():
+    data = bst.BST()
+    keys = [0, 1, 2, 3, 4, 5 ,6 ,7 , 8, 9, 10]
+
+    for i in keys:
+        data.put(i, str(i))
+
+    data.delete(10)
+    assert(data.isBalanced())
+    data.put(10, str(10))
+    j = 0
+    for i in data.get_all_keys():
+        assert(i == keys[j])
+        j += 1
+
+def test_insert_delete_3():
+    data = bst.BST()
+    keys = [0, 1, 2, 3, 4, 5 ,6 ,7 , 8, 9, 10]
+
+    for i in keys:
+        data.put(i, str(i))
+
+    data.delete(10)
+    assert(data.isBalanced())
+
+    for i in data.get_all_keys():
+        print(i)
+
+    j = 0
+    for i in data.get_all_keys():
+        assert(i == keys[j])
+        j += 1
+
+def test_insert_delete_4():
+    data = bst.BST()
+    keys = [0, 1, 2, 3, 4, 5 ,6 ,7 , 8, 9, 10]
+    keyso = [0, 1, 2, 3, 4, 5 ,6 ,7 , 8, 10]
+
+    for i in keys:
+        data.put(i, str(i))
+
+    data.delete(9)
+    assert(data.isBalanced())
+
+    for i in data.get_all_keys():
+        print(i)
+
+    j = 0
+    for i in data.get_all_keys():
+        assert(i == keyso[j])
+        j += 1
+
+def test_insert_delete_5():
+    data = bst.BST()
+    keys = [0, 1, 2, 3, 4, 5 ,6 ,7 , 8, 9, 10]
+
+    for i in keys:
+        data.put(i, str(i))
+
+    data.delete(0)
+    assert(data.isBalanced())
+
+    for i in data.get_all_keys():
+        print(i)
+
+    j = 1
     for i in data.get_all_keys():
         assert(i == keys[j])
         j += 1
