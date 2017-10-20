9ecfd865ea28c776b5417c45222b974ecad5f51b
Eduardo Spagiari <e.spagiari@gmail.com>
2017-10-20 12:04:39 -0200

    improve util

--- src/util.py
+++ src/util.py
@@ -4,12 +4,12 @@ EP = datetime.fromtimestamp(0)
 
 def date_to_float(d):
     if d == None:
-        DT = datetime.now()
+        DT = datetime.fromtimestamp(1)
     else:
         try:
             DT = datetime.strptime(d, '%Y%m%d%H%M%S.%f0')
         except:
-            DT = datetime.now()
+            DT = datetime.fromtimestamp(1)
     return (DT - EP).total_seconds()
 
 def float_to_date(fl):
